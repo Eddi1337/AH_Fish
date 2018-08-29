7389ba7c39a6339a024a79f292e28c0d3bdccb62
Eddi <em5g13@soton.ac.uk>
2018-08-29 15:54:00 +0100

    Fixed Issue with graph not updating on load

--- static/js/chart-fish-stats.js
+++ static/js/chart-fish-stats.js
@@ -2,127 +2,102 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-
-// Area Chart Example
 //VARS
-
 var time_list = []
 var avg_price_list = []
 var min_price_list = []
 var price_list_array =[]
 var ctx = document.getElementById("myAreaChart");
-//var var myLineChart = {}
-//init();
-//myLineChart.update();
+
+init()
+
 //On startup
-
-$(document).ready(function () {
-
-    load_json();
-    lineChart = render_chart(time_list,min_price_list,avg_price_list);
-    lineChart.resize();
-    $(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
-        console.log("tab changed");
-    });
-});
-
-/*
 function init(){
   load_json();
-
-  //Render
-  render_chart(time_list,min_price_list,avg_price_list);
-  //myLineChart.update();
-  //chart.update();
-  //myLineChart.update();
 }
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    var ev = document.createEvent('Event');
-    ev.initEvent('resize', true, true);
-    window.dispatchEvent(ev);
-  });
-*/
-
-
 
 function load_json(){
+  //Get Min fish items and add them to line graph
   $.getJSON("fish_min.json", function(JSON) {
     console.log(JSON); // this will show the info it in firebug console
-    //get_dates_and_val(json2)
-    //addData(myLineChart,time_list,price_list)
     for (i = 0; i < JSON.length; i++){
-      min_price_list.push(JSON[i].val)
+      
       time_list.push(JSON[i].time)
+      myLineChart.data.datasets[1].data[i] = JSON[i].val
       //update graph
+      myLineChart.update()
     }
   });
-  //Get AVG fish items
+  //Get AVG fish items and add them to line graph
   $.getJSON("fish_avg.json", function(JSON) {
-    console.log(JSON); // this will show the info it in firebug console
-    //get_dates_and_val(json1)
-    //addData(myLineChart,time_list,price_list)
+    console.log(JSON);     
     for (i = 0; i < JSON.length; i++){
       avg_price_list.push(JSON[i].val)
-      //time_list.push(JSON[i].time)
+      myLineChart.data.datasets[0].data[i] = JSON[i].val
       //update graph
+      myLineChart.update()
     }
   });
 }
 
-function render_chart(time_l,min_l,avg_l){
-  var myLineChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: time_l,
-        datasets: [{
-          label: "Avg Gold Price",
-          lineTension: 0.3,
-          //backgroundColor: "rgba(0,255,0,0.3)",
-          borderColor: "rgb(127, 191, 63)",
-          pointRadius: 5,
-          pointBackgroundColor: "rgb(127, 191, 63)",
-          pointBorderColor: "rgba(255,255,255,0.8)",
-          pointHoverRadius: 5,
-          //pointHoverBackgroundColor: "rgb(127, 191, 63)",
-          pointHitRadius: 50,
-          pointBorderWidth: 2,
-          data: avg_l,
-        },
-        {
-          label: "Min Gold Price",
-          lineTension: 0.3,
-          //backgroundColor: "rgba(2,117,216,0.2)",
-          borderColor: "rgba(2,117,216,1)",
-          pointRadius: 5,
-          pointBackgroundColor: "rgba(2,117,216,1)",
-          pointBorderColor: "rgba(255,255,255,0.8)",
-          pointHoverRadius: 5,
-          //pointHoverBackgroundColor: "rgba(2,117,216,1)",
-          pointHitRadius: 50,
-          pointBorderWidth: 2,
-          data: min_l,
+var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: time_list,
+      datasets: [{
+        label: "Avg Gold Price",
+        lineTension: 0.3,
+        //backgroundColor: "rgba(0,255,0,0.3)",
+        borderColor: "rgb(127, 191, 63)",
+        pointRadius: 5,
+        pointBackgroundColor: "rgb(127, 191, 63)",
+        pointBorderColor: "rgba(255,255,255,0.8)",
+        pointHoverRadius: 5,
+        //pointHoverBackgroundColor: "rgb(127, 191, 63)",
+        pointHitRadius: 50,
+        pointBorderWidth: 2,
+        data: avg_price_list,
+      },
+      {
+        label: "Min Gold Price",
+        lineTension: 0.3,
+        //backgroundColor: "rgba(2,117,216,0.2)",
+        borderColor: "rgba(2,117,216,1)",
+        pointRadius: 5,
+        pointBackgroundColor: "rgba(2,117,216,1)",
+        pointBorderColor: "rgba(255,255,255,0.8)",
+        pointHoverRadius: 5,
+        //pointHoverBackgroundColor: "rgba(2,117,216,1)",
+        pointHitRadius: 50,
+        pointBorderWidth: 2,
+        data: min_price_list,
+      }],
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'date'
+          },
+          gridLines: {
+            display: false
+          },
+          ticks: {
+            maxTicksLimit: 7
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            maxTicksLimit: 5
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, .125)",
+          }
         }],
       },
-      options: {
-        scales: {
-          xAxes: [{
-            time: {
-              unit: 'date'
-            },
-            gridLines: {
-              display: false
-            },
-          }],
-          yAxes: [{
-            gridLines: {
-              color: "rgba(0, 0, 0, .125)",
-            }
-          }],
-        },
-        legend: {
-          display: false
-        }
+      legend: {
+        display: false
       }
-    });
-  return myLineChart;
-}
+    }
+});
