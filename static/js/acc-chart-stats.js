55d3bddfc7d88ddf6e3285f39dc7ac2686715012
Eddi <em5g13@soton.ac.uk>
2018-09-09 05:10:55 +0100

    Fixed issue with multiple graphs (finally)
    Don't forget about global variables

--- static/js/acc-chart-stats.js
+++ static/js/acc-chart-stats.js
@@ -9,7 +9,8 @@ var min_price_list = []
 var price_list_array =[]
 
 var item_list_array = []
-var chart_array = []
+var chart_list = {}
+var page_elements = {}
 var imported_json = {}
 
 function init(passed_item_id){
@@ -17,79 +18,39 @@ function init(passed_item_id){
   console.log(item_list_array)
   $.getJSON("monitored.json", function(JSON) {
   imported_json = JSON   
+
+
   for (i = 0; i < item_list_array.length; i++){ 
-    var ctx = document.getElementById(passed_item_id[i].toString());
-    //console.log
-    chart = create_charts(passed_item_id[i],ctx)
-    chart_array.push(chart)
-    console.log("adding item "+passed_item_id[i])
-    console.log(chart)
-    if (chart){
-          chart.data.labels.pop();
-    chart.data.datasets[0].data.pop();
-    chart.data.datasets[1].data.pop();
-    }
-
-    //add_items(passed_item_id[i],chart,JSON)
-    for (var x in JSON){
-      //console.log("itteration " + x);
+    page_elements[passed_item_id[i]] = document.getElementById(passed_item_id[i].toString());
+    //Create a list of charts for each passed id?
+    //chart = create_charts(passed_item_id[i],ctx)
+    console.log("creating chart for item "+passed_item_id[i])
+    chart_list[passed_item_id[i]] = create_charts(passed_item_id[i],page_elements[passed_item_id[i]])
+    for (var x in JSON){    
+      
       if (JSON[x].item_id == passed_item_id[i]){
-        console.log("adding to "+ chart.id + " With " + JSON[x].avg_val + " from " + passed_item_id[i])
-        //console.log(chart.id);
-        //console.log("with data " + JSON[x].time + " "+ JSON[x].avg_val + " "+ JSON[x].min_val)
-        chart.data.labels.push(JSON[i].time);
-        chart.data.datasets[0].data.push(JSON[i].avg_val);
-        chart.data.datasets[1].data.push(JSON[i].min_val);
-        chart.update()
-      } 
-    }
 
-    }
-  });
-}
-/*
-function add_items(item,chart,JSON){
-    for (var x in JSON){
-      console.log(x)
-      if (JSON[x].item_id == item){
-        console.log("itteration " + x);
-        console.log("item " +JSON[x].item_id);
-        chart.data.labels.push(JSON[x].time);
-        chart.data.datasets[0].data[x] = JSON[x].avg_val;
-        chart.data.datasets[1].data[x] = JSON[x].min_val;
-        chart.update();
+        chart_list[passed_item_id[i]].data.labels.push(JSON[x].time);
+        //console.log("Adding time " +JSON[x].time + " for "+ passed_item_id[i]);
+        chart_list[passed_item_id[i]].data.datasets[0].data.push(JSON[x].avg_val);
+        //console.log("Adding avg " +JSON[x].avg_val);
+        chart_list[passed_item_id[i]].data.datasets[1].data.push(JSON[x].min_val);
+        //chart_list[passed_item_id[i]].chart.reset();
+        chart_list[passed_item_id[i]].chart.update();
+        }
       }
     }
-}
-/* nope
-function add_items(item,chart,JSON){
-    list_of_times = []
-    list_of_val_min = []
-    list_of_val_avg = []
-    for (e = 0; e < JSON.length; e++){
-      console.log(item)
-      if (JSON[e].item_id == item){
-        
-        list_of_times.unshift(JSON[e].time)
-        list_of_val_min.unshift(JSON[e].min_val)
-        list_of_val_avg.unshift(JSON[e].avg_val)
-        }
-    }
-    console.log(list_of_times)
-    for (var i = list_of_times.length - 1; i >= 0; i--) {
-      chart.data.labels.push(list_of_times[i])
-    }
-    chart.data.labels.push(list_of_times)
-    //console.log("Average item val " + JSON[e].avg_val + " for item " + JSON[e].item_id)
-    chart.data.datasets[0].data[i] = list_of_val_avg
-    chart.data.datasets[1].data[i] = list_of_val_min
-    //}
-    //}
-    //charts.data.labels.data[i] = (JSON[a].time);
-    chart.update()
+  //chart_list[6557].chart.data.datasets[1].data.push(14);
+  console.log(chart_list)
+  for (var x in chart_list){
+
+    console.log(passed_item_id[i])
+    //chart_list[passed_item_id[i]].chart.update();
+  }
+  });
+
 }
 
-*/
 function create_charts(item_id, chart_id){
   var item_id = new Chart(chart_id, {
       type: 'line',
@@ -107,7 +68,7 @@ function create_charts(item_id, chart_id){
           //pointHoverBackgroundColor: "rgb(127, 191, 63)",
           pointHitRadius: 50,
           pointBorderWidth: 2,
-          data: avg_price_list,
+          //data: [0],
         },
         {
           label: "Min Gold Price",
@@ -121,7 +82,7 @@ function create_charts(item_id, chart_id){
           //pointHoverBackgroundColor: "rgba(2,117,216,1)",
           pointHitRadius: 50,
           pointBorderWidth: 2,
-          data: min_price_list,
+          //data: [0],
         }],
       },
       options: {
